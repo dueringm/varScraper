6f3f02d422d058b0a8bcd15a151c90d1932996e7
mduering <mark.duering@uzh.ch>
2022-11-23 15:21:04 +0100

    adds directory access

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pprint
 import re
@@ -6,36 +7,47 @@ from bs4 import BeautifulSoup
 
 
 # Set directory here:
-directory = "server"
+master_directory = ""
 
-# Globals
-json_dict = {}
+# Iterate over folders in POC-Tests
+for folder_name in os.listdir(master_directory):
+    directory = os.path.join(master_directory, folder_name)
+    if os.path.isdir(directory):
 
+        # Iterate over files in folder
+        for filename in os.listdir(directory):
+            if filename.endswith(""):
 
-for filename in os.listdir(directory):
-    try:
-        filepath = os.path.join(directory, filename)
-        with open(filepath, 'r') as file:
+                json_dict = {}
+                try:
+                    filepath = os.path.join(directory, filename)
 
-            mandant = re.findall("(-.*?-)", filename, flags=re.DOTALL)[0][1:-1]
+                    # Parse file for scraping
+                    with open(filepath, 'r') as file:
+                        soup = BeautifulSoup(file, "html.parser")
 
-            soup = BeautifulSoup(file, "html.parser")
-            soup_string = str(soup.prettify())
+                    mandant = re.findall("(-.*?-)", filename, flags=re.DOTALL)[0][1:-1]
+                    var_tags = soup.find_all("poc-variable")
 
-            var_tags = soup.find_all("datavariable")
+                    json_dict[mandant] = {}
+                    var_dict = json_dict[mandant]
 
-            json_dict[mandant] = {}
-            var_dict = json_dict[mandant]
+                    for tag in var_tags:
+                        # If double value, add to array
+                        if tag['id'] in var_dict:
+                            var_dict[tag['id']].append(tag.text)
+                        # Else create new array entry
+                        else:
+                            var_dict[tag['id']] = [tag.text]
 
-            for tag in var_tags:
-                # If double value, add to array
-                if tag['id'] in var_dict:
-                    var_dict[tag['id']].append(tag.text)
-                # Else create new array entry
-                else:
-                    var_dict[tag['id']] = [tag.text]
-    except:
-        print("Error occured in file: ", filename)
+                    # Create JSON in POC-tests sub-folder
+                    write_path = directory + '/values.json'
+                    with open(write_path, 'w') as file_to_write:
+                        json.dump(json_dict, file_to_write, ensure_ascii=False, indent=4)
+                        print("JSON created at: ", write_path)
 
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(json_dict)
+                    pp = pprint.PrettyPrinter(indent=2)
+                    pp.pprint(json_dict)
+
+                except:
+                    print("Error occurred in file: ", filename)
